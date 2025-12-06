6ac7c750478e17f3fa8ef1bdbac0056057629183
Titux Metal (DEV) <github@lgdweb.fr>
2025-12-06 11:39:49 +0100

    feat(api): add email service for auth notifications

--- apps/api/src/auth/infrastructure/services/index.ts
+++ apps/api/src/auth/infrastructure/services/index.ts
@@ -1,3 +1,4 @@
-export { PasswordService } from './Password.service'
+export { EmailService } from './email.service'
 export { JwtService } from './Jwt.service'
+export { PasswordService } from './Password.service'
 export { TokenService } from './Token.service'
