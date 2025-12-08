9e844d5a3c4e95d5bffacf602d65d8be4b4d908d
Titux Metal (DEV) <github@lgdweb.fr>
2025-12-08 09:15:35 +0100

    refactor(api): remove obsolete auth infrastructure

--- apps/api/src/auth/infrastructure/repositories/index.ts
+++
@@ -1,1 +0,0 @@
-export { PrismaAuthUserRepository } from './PrismaAuthUser.repository'
--- apps/api/src/auth/infrastructure/services/index.ts
+++ apps/api/src/auth/infrastructure/services/index.ts
@@ -1,4 +1,1 @@
 export { EmailService } from './email.service'
-export { JwtService } from './Jwt.service'
-export { PasswordService } from './Password.service'
-export { TokenService } from './Token.service'
