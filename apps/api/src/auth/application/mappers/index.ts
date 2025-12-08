bdc3ebe788cba62307a96496ddb7a67344194ddf
Titux Metal (DEV) <github@lgdweb.fr>
2025-12-08 09:13:47 +0100

    refactor(api): remove obsolete auth application layer

--- apps/api/src/auth/application/mappers/index.ts
+++
@@ -1,1 +0,0 @@
-export { AuthUserMapper } from './AuthUser.mapper'
--- apps/api/src/auth/application/use-cases/index.ts
+++
@@ -1,3 +0,0 @@
-export { LoginUseCase, type LoginResult } from './Login.uc'
-export { RegisterUseCase, type RegisterResult } from './Register.uc'
-export { LogoutUseCase, type LogoutResult } from './Logout.uc'
