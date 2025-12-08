fb873cd36e637140c3074ea7e48aaa0e30831335
Titux Metal (DEV) <github@lgdweb.fr>
2025-12-08 11:22:35 +0100

    fix: add additional field to authClient

--- apps/web/src/lib/authClient.ts
+++ apps/web/src/lib/authClient.ts
@@ -1,9 +1,18 @@
-import { adminClient } from 'better-auth/client/plugins'
+import { adminClient, inferAdditionalFields } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.PUBLIC_API_URL || 'http://localhost:3000',
-  plugins: [adminClient()]
+  plugins: [
+    adminClient(),
+    inferAdditionalFields({
+      user: {
+        username: { type: 'string', required: true },
+        firstName: { type: 'string', required: false },
+        lastName: { type: 'string', required: false }
+      }
+    })
+  ]
 })
 
 export const { useSession, signIn, signUp, signOut, admin } = authClient
