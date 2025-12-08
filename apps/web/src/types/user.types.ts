b6e2973fa6c09a6b3cb2ed0e49eadeaddbf2dd1d
Titux Metal (DEV) <github@lgdweb.fr>
2025-12-08 10:39:39 +0100

    feat(web): update user types for better-auth

--- apps/web/src/types/user.types.ts
+++ apps/web/src/types/user.types.ts
@@ -1,10 +1,11 @@
 export type User = {
   id: string
   email: string
+  emailVerified: boolean
   username: string
   firstName: string | null
   lastName: string | null
-  confirmed: boolean
+  role: string
   createdAt: string
   updatedAt: string
 }
